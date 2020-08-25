939b37c2d1362d38c1a5a5a39607f0886424f8c1
Deepali-33 <deepali.tiwari030101@gmail.comgit config --global user.name Deepali-33>
2020-08-25 11:13:16 +0530

    "decision"

---
+++ wheel.js
@@ -0,0 +1,177 @@
+
+    console.log("click");
+
+
+function  fun1() {
+   
+    document.getElementById("b2").disabled = false;
+    document.getElementById("b3").disabled = true;
+    document.getElementById("b4").disabled = true;
+    document.getElementById("b5").disabled = true;
+    document.getElementById("b6").disabled = true;
+    document.getElementById("b7").disabled = true;
+    document.getElementById("b8").disabled = true;
+    document.getElementById("b9").disabled = true;
+    document.getElementById("b10").disabled = true;
+
+    
+    var b1 = window.prompt("What is the problem? ");
+        alert("Your answer is " + b1);
+        document.getElementById("b1").disabled = true;
+        
+}
+
+
+function  fun2() {
+    
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b3").disabled = false;
+    document.getElementById("b4").disabled = true;
+    document.getElementById("b5").disabled = true;
+    document.getElementById("b6").disabled = true;
+    document.getElementById("b7").disabled = true;
+    document.getElementById("b8").disabled = true;
+    document.getElementById("b9").disabled = true;
+    document.getElementById("b10").disabled = true;
+
+    var b2 = window.prompt("What is the choices? ");
+    alert("Your answer is " + b2);
+   
+    document.getElementById("b2").disabled = true;
+
+   
+}
+
+function  fun3() {
+    
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b2").disabled = true;
+    document.getElementById("b4").disabled = false;
+    document.getElementById("b5").disabled = true;
+    document.getElementById("b6").disabled = true;
+    document.getElementById("b7").disabled = true;
+    document.getElementById("b8").disabled = true;
+    document.getElementById("b9").disabled = true;
+    document.getElementById("b10").disabled = true;
+
+    var b3 = window.prompt("What is the consequences? ");
+    alert("Your answer is " + b3);
+    document.getElementById("b3").disabled = true;
+}
+
+function  fun4() {
+    
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b2").disabled = true;
+    document.getElementById("b3").disabled = true;
+    document.getElementById("b5").disabled = false;
+    document.getElementById("b6").disabled = true;
+    document.getElementById("b7").disabled = true;
+    document.getElementById("b8").disabled = true;
+    document.getElementById("b9").disabled = true;
+    document.getElementById("b10").disabled = true;
+
+    var b4 = window.prompt("What is the values? ");
+    alert("Your answer is " + b4);
+    document.getElementById("b4").disabled = true;
+}
+
+function  fun5() {
+    
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b2").disabled = true;
+    document.getElementById("b3").disabled = true;
+    document.getElementById("b4").disabled = true;
+    document.getElementById("b6").disabled = false;
+    document.getElementById("b7").disabled = true;
+    document.getElementById("b8").disabled = true;
+    document.getElementById("b9").disabled = true;
+    document.getElementById("b10").disabled = true;
+
+    var b5 = window.prompt("What is the feelings? ");
+    alert("Your answer is " + b5);
+    document.getElementById("b5").disabled = true;
+}
+
+function  fun6() {
+    
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b2").disabled = true;
+    document.getElementById("b3").disabled = true;
+    document.getElementById("b4").disabled = true;
+    document.getElementById("b5").disabled = true;
+    document.getElementById("b7").disabled = false;
+    document.getElementById("b8").disabled = true;
+    document.getElementById("b9").disabled = true;
+    document.getElementById("b10").disabled = true;
+
+    var b6 = window.prompt("can you share more info? ");
+    alert("Your answer is " + b6);
+    document.getElementById("b6").disabled = true;
+}
+
+function  fun7() {
+    
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b2").disabled = true;
+    document.getElementById("b3").disabled = true;
+    document.getElementById("b4").disabled = true;
+    document.getElementById("b5").disabled = true;
+    document.getElementById("b6").disabled = true;
+    document.getElementById("b8").disabled = false;
+    document.getElementById("b9").disabled = true;
+    document.getElementById("b10").disabled = true;
+
+    var b7 = window.prompt("What do you think who can help you? ");
+    alert("Your answer is " + b7);
+    document.getElementById("b7").disabled = true;
+}
+
+function  fun8() {
+   
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b2").disabled = true;
+    document.getElementById("b3").disabled = true;
+    document.getElementById("b4").disabled = true;
+    document.getElementById("b5").disabled = true;
+    document.getElementById("b6").disabled = true;
+    document.getElementById("b7").disabled = true;
+    document.getElementById("b9").disabled = false;
+    document.getElementById("b10").disabled = true;
+
+    var b8 = window.prompt("What is the decision? ");
+    alert("Your answer is " + b8);
+    document.getElementById("b8").disabled = true;
+}
+
+function  fun9() {
+    
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b2").disabled = true;
+    document.getElementById("b3").disabled = true;
+    document.getElementById("b4").disabled = true;
+    document.getElementById("b5").disabled = true;
+    document.getElementById("b6").disabled = true;
+    document.getElementById("b7").disabled = true;
+    document.getElementById("b8").disabled = true;
+    document.getElementById("b10").disabled = false;
+
+    var b9 = window.prompt("What is the assess decision? ");
+    alert("Your answer is " + b9);
+    document.getElementById("b9").disabled = true;
+}
+
+function  fun10() {
+    
+    document.getElementById("b1").disabled = true;
+    document.getElementById("b2").disabled = true;
+    document.getElementById("b3").disabled = true;
+    document.getElementById("b4").disabled = true;
+    document.getElementById("b5").disabled = true;
+    document.getElementById("b6").disabled = true;
+    document.getElementById("b7").disabled = true;
+    document.getElementById("b8").disabled = true;
+    document.getElementById("b9").disabled = true;
+    
+}
+
